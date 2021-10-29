cf70625916bc1e0b1ce6e0ea69860d13029a354d
max7 <max7@DESKTOP-DQ2AN4C>
2021-10-29 12:17:05 +0300

    Add project files.

---
+++ Notes/Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://www.zeon.ua/?lang=en");
+        }
+        async void OnButtonClicked2(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://support.zeon.ua/");
+        }
+      
+    }
+}
